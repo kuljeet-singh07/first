590b9ecceb2dba1051bf1cdfce7088959e28dee1
kuljeet-singh07 <158825092+kuljeet-singh07@users.noreply.github.com>
2024-07-25 21:35:03 +0530

    Delete C-15-array_destructuring directory

--- C-15-array_destructuring/script.js
+++
@@ -1,6 +0,0 @@
- 
- const myArray = ["value1","value2","value3","value4"];
- let [myvar1,myvar2, ...mynewArray] =myArray;
- console.log("value of myvar1 : ",myvar1)
- console.log("value of myvar2 : ",myvar2)
- console.log(mynewArray);
